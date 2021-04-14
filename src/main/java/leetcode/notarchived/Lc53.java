7ce6b91d78ad7d62597cedda039f409b836eeaa6
snorlaxa <1553141498@qq.com>
2021-04-14 10:59:36 +0800

    添加题解LC53

--- src/main/java/leetcode/notarchived/Lc53.java
+++ src/main/java/leetcode/notarchived/Lc53.java
@@ -4,7 +4,8 @@ package leetcode.notarchived;
  * @Author: Yzy
  * @Date: 2021/4/14 9:56
  * @题意: 最大子序和，求数组中和最大的子序列
- * @题解:
+ * @题解: 分治法可以做，但计算中间部分的过程太暴力，不太喜欢。
+ * 动态规划稍微优雅点
  */
 public class Lc53 {
 
