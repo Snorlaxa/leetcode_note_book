bee8b75f626d94ecd257d30a689780fcf5d90e1c
snorlaxa <1553141498@qq.com>
2021-05-18 01:16:52 +0800

    Lc34 更完善的解释

--- src/main/java/leetcode/notarchived/Lc34.java
+++ src/main/java/leetcode/notarchived/Lc34.java
@@ -31,15 +31,13 @@ public class Lc34 {
         }
         /**
          * 为什么是left？
-         * 因为在求左边界的过程且目标值存在时，最后一轮有两种情况，一种是left=mid，下一轮会查左边，right=mid-1，此时right必定小于target，所以需要选择left
-         * 【    为什么right=mid-1必定小于target？因为不会存在[left,mid]范围之外的target
-         *      如果出现[1-left,2,3,3-mid,3,4-right]这种情况，取值范围是左边[0,3]，这样就避免了边界在[left,mid]之外的情况，
-         *      而right到了范围之外，必然小于target  】；
-         * 另一种情况是left<mid-1，则right=mid-1=left,相等，下一轮跳出。
-         * 不可能存在mid=right的情况，因为mid=(left+right)/2。
-         * 即无论怎么样，left都会是
-         * 最后一轮，left与right只有重合或不重合两种情况，重合时，下一次移动left=right,right:=mid-1,则nums[right]将会小于target；
-         * 不重合，则前一轮必定mid=left，最后一轮right:=mid-1，right<mid=left，跳出。若前一轮mid>left，不可能存在mid-1<left这种不重合的情况。
+         * 因为在求左边界的过程且目标值存在时，最后一轮一定有left=mid。因为如果left<mid, 目标又是存在的，下一轮就要跳出了，必然在这一轮能找到，
+         * 所以mid=target，而将right:=mid-1会使得循环跳出，说明right<left，所以mid一定等于left。
+         * 因为最后一轮left=mid=target,下一轮会查左边，right=mid-1，此时right必定小于target，所以需要选择left
+         * 【  为什么right=mid-1必定小于target？因为边界不会在[left,mid]范围的左边。
+         *    如[1=left,2,3,3=mid,3,4=right]这种情况，取值范围是左边left到mid（包含），[0,3]，
+         *    即遇到mid=target时，左取值边界是不会移动的，
+         *    这样就避免了边界在[left,mid]左边的情况，而right到了范围左边，必然小于target  】
           */
         res[0] = left;
         left = 0;
