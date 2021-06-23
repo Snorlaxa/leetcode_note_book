99e38da2ed67fc85b3f21fc2e1953e30fbc60b78
余子毅 <yuziyi@xforceplus.com>
2021-06-24 00:52:11 +0800

    新增题解Lc04、Lc111 增加总结

---
+++ src/main/java/base/TreeNode.java
@@ -0,0 +1,23 @@
+package base;
+
+/**
+ * @Author: 余子毅
+ * @Date: 2021/6/23 0:53
+ */
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+
+    }
+
+    public TreeNode(int val) {
+
+    }
+}
---
+++ src/main/java/leetcode/notarchived/done/Lc111.java
@@ -0,0 +1,25 @@
+package leetcode.notarchived.done;
+
+import base.TreeNode;
+
+/**
+ * @Author: 余子毅
+ * @Date: 2021/6/23 20:47
+ * @题意: 二叉树的最小深度
+ */
+public class Lc111 {
+    public int solution(TreeNode root) {
+        if (root == null) return 0;
+        int l = solution(root.left);
+        int r = solution(root.right);
+        // 若两个都是0，说明到叶子节点，若有一方不是0，说明还没到叶子节点，继续返回
+        // 如果两方都有，返回小的那个
+        int res = (l == 0 || r == 0) ? Math.max(l, r) : Math.min(l, r);
+        // 加1表示访问到当前的节点
+        return res + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
